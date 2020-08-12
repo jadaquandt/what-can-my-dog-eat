4c9bb9445c01368c6952e18c3875ec29d51751b9
Jada Quandt <jadaquandt@gmail.com>
2020-08-12 18:19:51 -0400

    updated dogFoods file and started building out the accordion div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+// import Accordion from './Accordion';
 
 function App() {
   return (
     <div className="App">
+      {/* <Accordion /> */}
       <h1>Can my dog eat...?</h1>
     </div>
   );
