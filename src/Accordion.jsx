8c2dcbb104b185ca1bbc44a8db0430a5b5627ab8
Ashley Singleton <asingleton6@gmail.com>
2020-09-26 22:12:33 -0400

    add expand animation to accordion

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 export class Accordion extends Component {
     constructor() {
         super();
-
-        this.state = { showVeg: false,
-                    showSweet: false,
-                    showSalt: false
-         };
+        this.state = { 
+            showVeg: false,
+            showSweet: false,
+            showSalt: false
+        };
     }
 
     handleShowSalt = () => {
@@ -33,9 +33,8 @@ export class Accordion extends Component {
                     : <img class="arrows" src="/images/down-arrow-2.png" alt=""/>
                     }
                 </div>
-                { this.state.showVeg === true
-                    ? 
-                    <div class="infoInside">
+                <div className={this.state.showVeg === true ? 'infoInside expanded' : 'infoInside'}>
+                    <p>
                     Dogs are omnivores, which means they can eat both meat and plants. 
                     Many commercial dog foods are made with vegetables and fruits along 
                     with meat and grains. These products are also made to include all 
@@ -45,9 +44,8 @@ export class Accordion extends Component {
                     besides what's in her kibble, they won’t hurt her, either. Some 
                     produce can be part of good dog nutrition. Feed with caution though, 
                     always do your research before feeding your dog fruits and veggies.
-                    </div>
-                    : null
-                }
+                    </p>
+                </div>
             </div>
             <div className="info">
                 <div onClick={this.handleShowSalt} className="infoHeaders">
@@ -56,19 +54,17 @@ export class Accordion extends Component {
                     : <img className="arrows" src="/images/down-arrow-2.png" alt=""/>
                     }
                     </div>
-                { this.state.showSalt === true
-                    ? 
-                    <div className="infoInside">
-                    While the occasional salty snack probably won't hurt your dog, 
-                    always be aware of your pup's salt intake. When too much salt builds up 
-                    in a dog’s body, her cells release their water content to try and balance 
-                    out the sodium disparity. <br/> <br/> This, in turn, causes a litany of serious 
-                    health effects. It can cause seizures, a loss of brain cells, injury to 
-                    the kidneys, and severe dehydration. If a dog with salt poisoning 
-                    isn’t treated medically, the condition can easily lead to death.
+                    <div className={this.state.showSalt === true ? 'infoInside expanded' : 'infoInside'}>
+                        <p>
+                        While the occasional salty snack probably won't hurt your dog, 
+                        always be aware of your pup's salt intake. When too much salt builds up 
+                        in a dog’s body, her cells release their water content to try and balance 
+                        out the sodium disparity. <br/> <br/> This, in turn, causes a litany of serious 
+                        health effects. It can cause seizures, a loss of brain cells, injury to 
+                        the kidneys, and severe dehydration. If a dog with salt poisoning 
+                        isn’t treated medically, the condition can easily lead to death.
+                        </p>
                     </div>
-                    : null
-                }
             </div>
             <div id="info">
                 <div onClick={this.handleShowSweet} className="infoHeaders">
@@ -77,9 +73,8 @@ export class Accordion extends Component {
                     : <img className="arrows" src="/images/down-arrow-2.png" alt=""/>
                     }
                 </div>
-                    { this.state.showSweet === true
-                    ? 
-                    <div className="infoInside">
+                <div className={this.state.showSweet === true ? 'infoInside expanded' : 'infoInside'}>
+                    <p>
                         Both chocolate and the artificial sweetener xylitol (found in many 
                         sugar-free candies) can be toxic to dogs. Never give these to your dog. 
                         Other than that, sugary foods that humans like to eat are generally not 
@@ -89,9 +84,8 @@ export class Accordion extends Component {
                         weight gain, metabolic changes, and even diabetes in dogs. We know it's 
                         hard to say no to those puppy eyes when they're staring down your ice cream, 
                         but it's for her good!
-                    </div>
-                    : null
-                }
+                    </p>
+                </div>
             </div>
             </div>
         )
